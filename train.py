6ac5f3fba00286f37417c2fd703082a7ca8f4864
Javier Rando Ramírez <javier.rando01@estudiant.upf.edu>
2022-03-24 15:32:35 +0100

    - New training script

---
+++ train.py
@@ -0,0 +1,115 @@
+from torch.utils.data import Dataset, DataLoader
+from datetime import datetime
+import os
+from torch.utils.tensorboard import SummaryWriter
+import unet
+import torch
+import argparse
+
+def train(model: str,
+          model_config: dict,
+          loss: callable,
+          optimizer: callable,
+          n_epochs: int,
+          train_dataloader: DataLoader,
+          test_dataloader: DataLoader,
+          model_save_path: str,
+          logs_save_path: str,
+          save_freq: int = None,
+          device: str = "cpu",
+          logging_freq = 100):
+    """
+    Function used to call a model for the CIL project
+    :param model: string among the implemented models. Options: "unet"
+    :param model_config: dictionary containing the parameters for the model
+    :param loss: function used to compute the loss. Takes as input (prediction, target)
+    :param optimizer: optimizer for the model from torch.optim
+    :param n_epochs: number of epochs to train for
+    :param batch_size: training batch size
+    :param train_dataloader: Dataloader containing training data
+    :param test_dataloader: Dataloader containing testing data
+    :param model_save_path: Path where model checkpoints will be stored
+    :param logs_save_path: Path where training logs will be stored
+    :param save_freq: Frequency used to store the model
+    :param device: Device used for training. Options: "cuda" or "cpu"
+    """
+
+    # Define a run id for the execution
+    run_id = datetime.now().strftime("%Y-%m-%d_%H-%M")
+    print(f"Starting training a {model} architecture with id {run_id} on {device}")
+
+    # Create storing folders if they do not exist
+    for d in [logs_save_path, model_save_path]:
+        if not os.path.exists(d):
+            os.makedirs(d)
+
+    # Define a summary writer for the logs
+    writer = SummaryWriter(os.path.join(logs_save_path, run_id))
+
+    # Get model from its name
+    if model == "unet":
+        model = unet.UNet(**model_config).to(device)
+
+    # Training process
+    optimizer = optimizer(model.parameters())
+    for epoch in range(n_epochs):
+        epoch_loss = 0
+        model.train()
+
+        for i, (inp, target) in enumerate(train_dataloader):
+
+            if epoch == 0 and i==0:
+                writer.add_graph(model, inp)
+
+            # move input to cuda if required
+            if device == "cuda":
+                inp = inp.cuda(non_blocking=True)
+                target = target.cuda(non_blocking=True)
+
+            # forward pass
+            pred = model.forward(inp)
+            loss = loss(pred, target)
+
+            # compute the gradients
+            optimizer.zero_grad()
+            loss.backward()
+
+            # optimizer step
+            optimizer.step()
+
+            # log
+            epoch_loss += loss.item()
+
+            if i % logging_freq == 0:
+                print(f"Epoch {epoch+1}/{n_epochs} | Batch {i}/{len(train_dataloader)} => Running epoch loss per sample: {epoch_loss/(logging_freq*train_dataloader.batch_size)}")
+
+        # Log the loss
+        writer.add_scalar("Loss/train", epoch_loss/len(train_dataloader.dataset), epoch)
+
+        # Store model
+        if save_freq is not None and ((epoch + 1) % save_freq == 0 or epoch + 1 == n_epochs):
+            checkpoint_name = f"{run_id}_epoch{epoch + 1}"
+            checkpoint_path = f"./model/{checkpoint_name}.pkl"
+            print(f"Saving model to {checkpoint_path}")
+            torch.save(model, checkpoint_path)
+
+        if test_dataloader:
+            model.eval()
+            eval_loss = 0.
+
+            with torch.no_grad():
+                for i, (inp, target) in enumerate(test_dataloader):
+                    # move input to cuda if required
+                    if device == "cuda":
+                        inp = inp.cuda(non_blocking=True)
+                        target = target.cuda(non_blocking=True)
+
+                    # forward pass
+                    pred = model.forward(inp)
+                    loss = loss(pred, target)
+                    eval_loss += loss.item()
+
+                writer.add_scalar("Loss/eval", eval_loss / len(test_dataloader.dataset), epoch)
+                print(f"Evaluation loss after epoch {epoch+1}/{n_epochs}: {eval_loss / len(test_dataloader.dataset)}")
+
+        writer.flush()
--- unet.py
+++ unet.py
@@ -67,7 +67,7 @@ class UNetDecoder(torch.nn.Module):
         return x
 
 class UNet(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, encoder_layers=2):
         super().__init__()
         self.encoder = UNetEncoder([
             UNetLevel(layers=2, dim_coef=1, in_channels=3, out_channels=64),
@@ -84,12 +84,9 @@ class UNet(torch.nn.Module):
         ])
         self.decoder_top = torch.nn.Sequential(collections.OrderedDict([ #TODO: consider using a single output channel like the paper
             ("1x1_conv", torch.nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1, padding="same")),
-            #no softmax here, should be handled by the loss itself
         ]))
-        self.loss = torch.nn.CrossEntropyLoss()
-        self.optimizer = torch.optim.Adam(self.parameters())
-        self.epoch = 0
-        self.run_id = datetime.now().strftime("%Y-%m-%d_%H-%M")
+
+
     def forward(self, x):
         x, outs = self.encoder(x)
         outs.pop(-1) #no resisual connection from the last encoder level
@@ -97,58 +94,7 @@ class UNet(torch.nn.Module):
         x = self.decoder(x, outs)
         x = self.decoder_top(x)
         return x
-    def custom_train(self, train_data, dev_data, epochs, save_freq=None):
-        print(f"starting training with run id {self.run_id}")
-        for d in ["./model", "./logs"]:
-            if not os.path.exists(d): os.makedirs(d)
-        writer = SummaryWriter(os.path.join("logs", self.run_id))
-        for e in range(self.epoch, epochs):
-            epoch_loss = 0
-            for idx, (x, y) in enumerate(train_data):
-                if e == 0 and idx == 0: writer.add_graph(self, x)
-                pred = self.forward(x)
-                loss = self.loss(pred, y)
-                epoch_loss += loss
-                print(f"epoch {e+1}, batch {idx+1}/{len(train_data)} loss: {loss}")
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-            epoch_loss /= len(train_data)
-            writer.add_scalar("loss", epoch_loss, e)
-            if save_freq is not None and ((e+1) % save_freq == 0 or e+1 == epochs):
-                self.epoch = e+1
-                checkpoint_name = f"{self.run_id}_epoch{e+1}"
-                checkpoint_path = f"./model/{checkpoint_name}.pkl"
-                print(f"saving model to {checkpoint_path}")
-                torch.save(self, checkpoint_path)
 
-        writer.flush()
-
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self, img_dir, groundtruth_dir, start, end):
-        self.img_dir, self.groundtruth_dir = img_dir, groundtruth_dir
-        self.img_list = os.listdir(img_dir)
-        try:
-            foo = Dataset.permutation
-        except AttributeError: #generate a random permutation to achieve a random split between train and test data
-            generator = np.random.Generator(np.random.PCG64(42))
-            perm = np.array(list(range(len(self.img_list))))
-            generator.shuffle(perm)
-            Dataset.permutation = perm
-        self.img_list = [self.img_list[i] for i in Dataset.permutation]
-        n_examples = len(self.img_list)
-        if end is not None:
-            self.img_list = self.img_list[start:end]
-        else:
-            self.img_list = self.img_list[start:]
-    def __len__(self):
-        return len(self.img_list)
-    def __getitem__(self, idx):
-        x, y = [np.array(imageio.imread(os.path.join(im_dir, self.img_list[idx]))) for im_dir in [self.img_dir, self.groundtruth_dir]]
-        x = torch.tensor(np.transpose(x[:, :, :3], [2, 0, 1]), dtype=torch.float32) #TODO: float64?
-        y = torch.tensor(y, dtype=torch.float32) / 255
-        y = torch.stack([y, 1-y], dim=0)
-        return x.to(device), y.to(device)
 
 img_dir, groundtruth_dir = "./data/training/images", "./data/training/groundtruth"
 train_dataset, dev_dataset = Dataset(img_dir, groundtruth_dir, 10, None), Dataset(img_dir, groundtruth_dir, 0, 10)
