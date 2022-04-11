d9fe0b8b722b20e32772544614c3d08ef37a7d75
Javier Rando Ramírez <javier.rando01@estudiant.upf.edu>
2022-04-11 15:08:16 +0200

    - new transformations

--- datautil.py
+++ datautil.py
@@ -104,11 +104,11 @@ default_base_transformations = {IdentityTransform(): 0.25,
                                 SegRotationTransform(): 0.25}
 
 # Then these will be applied on top of the previous ones
-default_additional_transformations = {IdentityTransform(): 0.30,
-                                      SegRotationTransform(): 0.35,
-                                      SegAdjustBrightness(): 0.15,
-                                      SegGaussianBlur(): 0.05,
-                                      SegAdjustContrast(): 0.15}
+default_additional_transformations = {IdentityTransform(): 0.3,
+                                      SegRotationTransform(): 0.2,
+                                      SegHorizontalFlip(): 0.2,
+                                      SegVerticalFlip(): 0.2,
+                                      SegAdjustBrightness(): 0.1}
 
 
 class CILRoadSegmentationDataset(Dataset):
